785ff20a4e86a1091a62a07586e2e9d624138abd
Paul Thompson <mailto.prt@gmail.com>
2020-08-21 09:07:55 -0400

    removing evil icons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { bind, wire } from "hyperhtml";
-const icons = require('evil-icons');
 
 const q = (selector) => document.querySelector(selector);
 const qa = (selector) => document.querySelectorAll(selector);
@@ -9,15 +8,27 @@ const app = q('#app');
 
 const logo = q('.logo');
 const installBtn = q('.install');
-const nav = q('.nav');
+
+const menu = q('.menu');
+const renderMenu = bind(menu);
+
+const pages = ["schedule", "speakers", "partners", "make my day", "resources"];
+const menuItems = pages.map(item => {
+    return wire()`<div class="item">
+        <p>${item}</p>
+    </div>
+    `;
+});
+renderMenu`${menuItems.map(item => item)}`;
+
 
 logo.addEventListener('animationend', e => {
     logo.classList.add('d-none');
 })
 
-setTimeout(() =>{
+setTimeout(() => {
     logo.classList.add('fade-out');
-},2000);
+}, 2000);
 
 
 installBtn.addEventListener('click', e => {
