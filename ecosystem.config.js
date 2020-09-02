4c7f12ced8cd5d6c45e6ad529a66ee574c030209
Paul Thompson <mailto.prt@gmail.com>
2020-09-02 11:28:08 -0400

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,9 @@ module.exports = {
             name:"server",
             script:"./server.js",
             watch: true,
+            error_file:"./logs/error.log",
+            out_file:"./logs/info.log",
+            log_file:"./logs/combined.log"
             env: {
                 "NODE_ENV": "production"
             }
