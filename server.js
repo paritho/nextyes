9c861923c38b96a18588509a5ead80ea60423cbb
Paul Thompson <mailto.prt@gmail.com>
2020-09-02 10:41:06 -0400

    moving try block

--- server.js
+++ server.js
@@ -55,51 +55,51 @@ server.get(["/schedule", "/schedule.html"], (req, res) => {
     res.sendFile(pathJoiner('public/views/schedule.html'));
 })
 server.get("/cookieSignon/:hash", (req, res) => {
-    if(!req.params.hash){
+    if (!req.params.hash) {
         logger.info(`get request with no hash`)
-        return res.status(403).send({noop: "signon not permitted"})
+        return res.status(403).send({ noop: "signon not permitted" })
     }
     const publicHash = req.params.hash;
     const auths = require("./db/authHash.json");
     const privateHash = auths[publicHash];
-    if(!privateHash){
-        res.status(403).send({failed:'no hash match'});
+    if (!privateHash) {
+        res.status(403).send({ failed: 'no hash match' });
         return;
     }
     const result = login(privateHash.hash);
     if (result.success) {
         //set cookie, expires in 3 days
         res.cookie('user', publicHash, { maxAge: 3 * 24 * 60 * 60 * 1000 })
-        res.send({ success:'login' });
+        res.send({ success: 'login' });
     }
-    if(result.failed){
+    if (result.failed) {
         logger.error(`cookieSignon failed`)
-        res.status(403).send({failed:'signon failed'});
+        res.status(403).send({ failed: 'signon failed' });
     }
 })
 
-server.post("/signon", (req, res) =>{
+server.post("/signon", (req, res) => {
     const publicHash = hash(`${req.body.email}`);
     const auths = require("./db/authHash.json");
     const privateHash = auths[publicHash];
-    if(!privateHash){
-        res.status(403).send({failed:'no matching user'});
+    if (!privateHash) {
+        res.status(403).send({ failed: 'no matching user' });
         return;
     }
 
     const givenHash = shaHash(`${privateHash.salt}${req.body.password}`);
-    if(givenHash !== privateHash.hash){
-        res.status(403).send({failed:'private hash missmatch'});
+    if (givenHash !== privateHash.hash) {
+        res.status(403).send({ failed: 'private hash missmatch' });
         return;
     }
     const result = login(privateHash.hash);
     if (result.success) {
         //set cookie, expires in 3 days
         res.cookie('user', publicHash, { maxAge: 3 * 24 * 60 * 60 * 1000 })
-        res.send({ success:'login'});
+        res.send({ success: 'login' });
     }
-    if(result.failed){
-        res.status(403).send({failed:'signon failed'});
+    if (result.failed) {
+        res.status(403).send({ failed: 'signon failed' });
     }
 })
 
@@ -109,21 +109,21 @@ server.post('/signup', (req, res) => {
         lastName: req.body.lastName,
         email: req.body.email
     };
-    const auths = require("./db/authHash.json");
-    const publicHash = hash(`${req.body.email}`);
-
-    if(auths[publicHash]){
-        return res.send({dupemail:'user already exists'});
-    }
-
-    const salts = salt();
-    const privateHash = {
-        salt:salts,
-        hash: shaHash(`${salts}${req.body.password}`)
-    };
-
-    auths[publicHash] = privateHash;
     try {
+        const auths = require("./db/authHash.json");
+        const publicHash = hash(`${req.body.email}`);
+
+        if (auths[publicHash]) {
+            return res.send({ dupemail: 'user already exists' });
+        }
+
+        const salts = salt();
+        const privateHash = {
+            salt: salts,
+            hash: shaHash(`${salts}${req.body.password}`)
+        };
+
+        auths[publicHash] = privateHash;
         fs.writeFile(pathJoiner("db/authHash.json"), JSON.stringify(auths), error => {
             if (error) {
                 logger.error('Problem writing private hashes')
@@ -132,7 +132,7 @@ server.post('/signup', (req, res) => {
                 logger.info('Set private hashes for new user.')
             }
         });
-    } catch (err){
+    } catch (err) {
         logger.error('Error writing to db', err)
     }
 
@@ -140,8 +140,8 @@ server.post('/signup', (req, res) => {
     if (result.success) {
         res.cookie('user', publicHash, { maxAge: 3 * 24 * 60 * 60 * 1000 })
         return res.send(result);
-    } 
-    if(result.failed){
+    }
+    if (result.failed) {
         logger.error(`failed to register user ${userdata.email}`)
         res.status(403).send({ failed: 'Error registering new user' });
     }
