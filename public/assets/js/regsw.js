a3d22e416718cf5908c09dcc5f783ee0116fd615
Paul Thompson <mailto.prt@gmail.com>
2020-09-09 11:36:16 -0400

    remove sw from other pages

--- public/assets/js/regsw.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/worker')
-            .then((reg) => {
-                console.log('Service worker registered.', reg);
-            });
-    });
-}
