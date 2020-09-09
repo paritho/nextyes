bcb8de11f36f996d8654ad7fc6ad1b3918e45080
Paul Thompson <mailto.prt@gmail.com>
2020-09-09 11:20:43 -0400

    install btn

--- src/content.js
+++ src/content.js
@@ -8,10 +8,41 @@ const q = (selector) => document.querySelector(selector);
 const qa = (selector) => document.querySelectorAll(selector);
 const app = q('#app');
 
+let displayMode = 'browser tab';
+window.addEventListener('DOMContentLoaded', () => {
+    if (navigator.standalone || window.matchMedia('(display-mode: standalone)').matches){
+        displayMode = 'standalone';
+    }
+});
+
 renderMenu().then(() => {
     const backBtns = q('.back-btns');
+    const contact = q('.contact');
+    const installBtn = q('.install');
     backBtns.addEventListener('click', e => {
         window.history.go(-1)
     })
+    contact.addEventListener('click', e => {
+        window.location.href = "/contact";
+    })
+
+    let install = "";
+    if (displayMode !== 'standalone') {
+        window.addEventListener('beforeinstallprompt', e => {
+            install = e;
+            installBtn.classList.remove('d-none');
+        });
+
+        window.addEventListener('appinstalled', e => {
+            // logging?
+        })
+
+        installBtn.addEventListener('click', e => {
+            install.prompt();
+            window.location.reload(false);
+        });
+    }
+
     bringIn(backBtns);
+    bringIn(contact);
 })
